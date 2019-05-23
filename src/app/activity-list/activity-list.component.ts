f832419b5c22425b94f1ea664f9f97ff6715b19a
Weston Harper <wes@tao.team>
2019-05-23 11:45:13 -0600

    added comments to activities component

--- src/app/activity-list/activity-list.component.ts
+++ src/app/activity-list/activity-list.component.ts
@@ -12,10 +12,13 @@ export class ActivityListComponent implements OnInit {
   constructor(private activityService: ActivitiesService) { }
 
   ngOnInit() {
-    this.activityService.getActivities();
+    // listen for updates to activities$
     this.activityService.activities$
       .subscribe((data) => {
         this.activities = data;
       });
+
+    // make the service update activities$
+    this.activityService.getActivities();
   }
 }
