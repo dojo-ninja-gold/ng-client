12edf45b9b9548c5dc9a5ca2c0bc1c774f49310e
Weston Harper <wes@tao.team>
2019-05-23 17:06:49 -0600

    added registration data binding

--- src/app/login-reg/login-reg.component.ts
+++ src/app/login-reg/login-reg.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-reg.component.css']
 })
 export class LoginRegComponent implements OnInit {
+  registerData: object = {
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+  }
+
+  loginForm: object = {
+    email: '',
+    password: '',
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  submitRegister() {
+    console.log(this.registerData);
+  }
 }
