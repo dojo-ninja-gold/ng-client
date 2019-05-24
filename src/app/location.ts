1e595ee615807842290b0bf96b9c282e133249c8
Weston Harper <wes@tao.team>
2019-05-24 15:54:23 -0600

    added location class

--- src/app/location.ts
+++ src/app/location.ts
@@ -1,2 +1,11 @@
 export class Location {
+  model: string;
+  pk: number;
+  fields: {
+    name: string;
+    min_gold: number;
+    max_gold: number;
+    created_at: number;
+    updated_at: number
+  }
 }
