297a084a256d11c4e40096c99795e56d08261cfb
Weston Harper <wes@tao.team>
2019-05-24 16:04:39 -0600

    added register and login interfaces for form warnings

--- src/app/user.ts
+++ src/app/user.ts
@@ -3,3 +3,15 @@ export class User {
   gold: number;
   first_name: string;
 }
+
+export interface RegisterData {
+  first_name: null | string;
+  last_name: null | string;
+  email: null | string;
+  password: null | string;
+}
+
+export interface LoginData {
+  email: null | string;
+  password: null | string;
+}
