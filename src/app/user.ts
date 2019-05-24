2692ed7f9bc2ed9cb9ba767e5670f8e8a581eed9
Weston Harper <wes@tao.team>
2019-05-24 15:55:22 -0600

    update user class to reflect server data format

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,13 +1,5 @@
 export class User {
-  model: string;
-  pk: 1;
-  fields: {
-    first_name: string;
-    last_name: string;
-    email: string;
-    gold: number;
-    pw_hash: string;
-    created_at: number;
-    updated_at: number;
-  }
+  id: number;
+  gold: number;
+  first_name: string;
 }
